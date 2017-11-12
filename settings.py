e4ceb77e2af00bef6c846a3126d1e038389cf556
Christian <christig@ucm.es>
2017-11-12 18:33:27 +0100

    Creada API REST para interactuar con la BD del servidor

---
+++ settings.py
@@ -0,0 +1,59 @@
+RESOURCE_METHODS = ['GET','POST']
+
+ITEM_METHODS = ['GET','PUT']
+
+DOMAIN = {'temblores': {
+                'additional_lookup': {
+                    'url': 'regex("[\w]+")',
+                    'field': 'id'
+                },
+                'schema':{
+                    'id':{
+                        'type': 'integer'
+                    },
+                    'duracion':{
+                        'type': 'integer'
+                    },
+                    'observaciones':{
+                        'type': 'string'
+                    },
+                    'timestamp_inicio':{
+                        'type': 'timestamp'
+                    }
+                }
+            },
+          'datos_sensor': {
+                'additional_lookup': {
+                    'url': 'regex("[\w]+")',
+                    'field': 'posiciones'
+                },
+                'schema': {
+                    'posiciones': {
+                        'type': 'integer'
+                    },
+                    'db_timestamp': {
+                        'type': 'timestamp'
+                    },
+                    'app_timestamp': {
+                        'type': 'timestamp'
+                    },
+                    'datos': {
+                        'type': 'string'
+                    }
+                }
+          },
+          'posiciones': {
+                'additional_lookup': {
+                    'url': 'regex("[\w]+")',
+                    'field': 'id'
+                },
+                'schema': {
+                    'id': {
+                        'type': 'integer'
+                    },
+                    'posiciones': {
+                        'type': 'string'
+                    }
+                }
+          }
+         }
