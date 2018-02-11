3d03fa09f1d25148f5be241d477959f33f7e12a8
Christian <christig@ucm.es>
2018-02-11 16:16:02 +0100

    añadidas las tablas de actividades y medicamentos

--- settings.py
+++ settings.py
@@ -70,5 +70,48 @@ DOMAIN = {'temblores': {
                         'type': 'string'
                     }
                 }
+          },
+          'medicamentos': {
+                'additional_lookup': {
+                    'url': 'regex("[\w]+")',
+                    'field': 'nombre'
+                },
+                'schema': {
+                    'nombre': {
+                        'type': 'string'
+                    },
+                    'dias': {
+                        'type': 'string'
+                    },
+                    'hora': {
+                        'type': 'string'
+                    },
+                    'minutos_descartar': {
+                        'type': 'integer'
+                    },
+                    'device_id':{
+                        'type': 'string'
+                    }
+                }
+          },
+          'actividades': {
+                'additional_lookup': {
+                    'url': 'regex("[\w]+")',
+                    'field': 'nombre'
+                },
+                'schema':{
+                    'nombre': {
+                        'type': 'string'
+                    },
+                    'intervalo': {
+                        'type': 'integer'
+                    },
+                    'hora': {
+                        'type': 'string'
+                    },
+                    'device_id':{
+                        'type': 'string'
+                    }
+                }
           }
-         }
+}
