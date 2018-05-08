7fece9b2d835edd2a7a14a395a5ae96c973368c3
Christian <christig@ucm.es>
2018-05-08 22:00:10 +0200

    Updated deploy.sh

--- docker-infrastructure/deploy.sh
+++ docker-infrastructure/deploy.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Ilegal number of arguments"
+	echo "sudo ./deploy.sh <api_port>"
+else
+	docker build -t tfgparkinson_api images/api/
+	docker run --name="database" mongo &
+	sleep 10
+	docker run --link database --name="api" -p $1:5050 tfgparkinson_api &
+fi
