744f57a989b896894b198741e6d64c41aac10be1
Christian <christig@ucm.es>
2018-05-13 20:26:03 +0200

    Added infrastructure for webserver
    Co-authored-by: al3xhh <al3xhh1995@gmail.com>

--- docker-infrastructure/deploy.sh
+++ docker-infrastructure/deploy.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
 	echo "Ilegal number of arguments"
-	echo "sudo ./deploy.sh <api_port>"
+	echo "sudo ./deploy.sh <api_port> <web_port>"
 else
-	docker build -t tfgparkinson_api images/api/
-	docker run --name="database" mongo &
+	docker build -t tfgparkinson_api images/api/	
+
+	git clone https://github.com/al3xhh/TFGParkinsonWeb.git ./images/webserver/TFGParkinsonWeb	
+
+	docker build -t tfgparkinson_server images/webserver
+
+	docker run -d --name="database" mongo
 	sleep 10
-	docker run --link database --name="api" -p $1:5050 tfgparkinson_api &
+	docker run -d --link database --name="api" -p $1:5050 tfgparkinson_api
+	sleep 10 
+	docker run -d --link api --name="web_server" -p $2:80 tfgparkinson_server
+	sleep 10
+	rm -rf ./images/webserver/TFGParkinsonWeb
 fi
